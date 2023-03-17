e55a64ea7fbe945657446eb87ad2fa79b2739fbf
Tyler Weaver <tyler@picknik.ai>
2023-03-17 11:32:22 -0600

    Rebase from 'upstream'

--- moveit_planners/pilz_industrial_motion_planner/src/command_list_manager.cpp
+++ moveit_planners/pilz_industrial_motion_planner/src/command_list_manager.cpp
@@ -32,7 +32,7 @@
  *  POSSIBILITY OF SUCH DAMAGE.
  *********************************************************************/
 
-#include "pilz_industrial_motion_planner/command_list_manager.h"
+#include <pilz_industrial_motion_planner/command_list_manager.h>
 
 #include <rclcpp/logger.hpp>
 #include <rclcpp/logging.hpp>
@@ -44,10 +44,10 @@
 #include <moveit/robot_state/conversions.h>
 
 #include "cartesian_limits_parameters.hpp"
-#include "pilz_industrial_motion_planner/joint_limits_aggregator.h"
-#include "pilz_industrial_motion_planner/tip_frame_getter.h"
-#include "pilz_industrial_motion_planner/trajectory_blend_request.h"
-#include "pilz_industrial_motion_planner/trajectory_blender_transition_window.h"
+#include <pilz_industrial_motion_planner/joint_limits_aggregator.h>
+#include <pilz_industrial_motion_planner/tip_frame_getter.h>
+#include <pilz_industrial_motion_planner/trajectory_blend_request.h>
+#include <pilz_industrial_motion_planner/trajectory_blender_transition_window.h>
 
 namespace pilz_industrial_motion_planner
 {
@@ -247,7 +247,7 @@ CommandListManager::solveSequenceItems(const planning_scene::PlanningSceneConstP
       throw PlanningPipelineException(os.str(), res.error_code_.val);
     }
     motion_plan_responses.emplace_back(res);
-    RCLCPP_DEBUG_STREAM(LOGGER, "Solved [" << ++curr_req_index << "/" << num_req << "]");
+    RCLCPP_DEBUG_STREAM(LOGGER, "Solved [" << ++curr_req_index << '/' << num_req << ']');
   }
   return motion_plan_responses;
 }
