3d9d7a8307d53d91d288143e7465d658271bd8e2
Tyler Weaver <tyler@picknik.ai>
2023-03-17 11:32:17 -0600

    Rebase from 'upstream'

--- moveit_plugins/moveit_simple_controller_manager/include/moveit_simple_controller_manager/gripper_controller_handle.h
+++ moveit_plugins/moveit_simple_controller_manager/include/moveit_simple_controller_manager/gripper_controller_handle.h
@@ -150,7 +150,7 @@ public:
     rclcpp_action::Client<control_msgs::action::GripperCommand>::SendGoalOptions send_goal_options;
     // Active callback
     send_goal_options.goal_response_callback =
-        [this](rclcpp_action::Client<control_msgs::action::GripperCommand>::GoalHandle::SharedPtr
+        [this](const rclcpp_action::Client<control_msgs::action::GripperCommand>::GoalHandle::SharedPtr&
                /* unused-arg */) { RCLCPP_DEBUG_STREAM(LOGGER, name_ << " started execution"); };
     // Send goal
     auto current_goal_future = controller_action_client_->async_send_goal(goal, send_goal_options);
@@ -194,9 +194,13 @@ private:
       const rclcpp_action::ClientGoalHandle<control_msgs::action::GripperCommand>::WrappedResult& wrapped_result) override
   {
     if (wrapped_result.code == rclcpp_action::ResultCode::ABORTED && allow_failure_)
+    {
       finishControllerExecution(rclcpp_action::ResultCode::SUCCEEDED);
+    }
     else
+    {
       finishControllerExecution(wrapped_result.code);
+    }
   }
 
   /*
