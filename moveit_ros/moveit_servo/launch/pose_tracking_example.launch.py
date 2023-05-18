dbdcafb6d7427f5bdb41a4156194338a4904dbac
Tyler Weaver <tyler@picknik.ai>
2023-05-18 12:15:00 -0600

    Rebase from 'upstream'

--- moveit_ros/moveit_servo/launch/pose_tracking_example.launch.py
+++ moveit_ros/moveit_servo/launch/pose_tracking_example.launch.py
@@ -14,9 +14,14 @@ def generate_launch_description():
     )
 
     # Get parameters for the Pose Tracking node
-    servo_params = {
+    pose_tracker_params = {
         "moveit_servo": ParameterBuilder("moveit_servo")
         .yaml("config/pose_tracking_settings.yaml")
+        .to_dict()
+    }
+
+    servo_params = {
+        "moveit_servo": ParameterBuilder("moveit_servo")
         .yaml("config/panda_simulated_config_pose_tracking.yaml")
         .to_dict()
     }
@@ -58,10 +63,7 @@ def generate_launch_description():
         executable="servo_pose_tracking_demo",
         # prefix=['xterm -e gdb -ex run --args'],
         output="screen",
-        parameters=[
-            moveit_config.to_dict(),
-            servo_params,
-        ],
+        parameters=[moveit_config.to_dict(), servo_params, pose_tracker_params],
     )
 
     # ros2_control using FakeSystem as hardware
