40546fb9f466bdc479b2ff0deb21a74319e84766
Henning Kayser <henningkayser@picknik.ai>
2023-09-10 22:40:01 +0200

    Rebase from 'upstream'

--- moveit_core/kinematic_constraints/src/utils.cpp
+++ moveit_core/kinematic_constraints/src/utils.cpp
@@ -42,7 +42,7 @@
 #include <rclcpp/node.hpp>
 #include <rclcpp/parameter_value.hpp>
 
-#include "rclcpp/node.hpp"
+#include <rclcpp/node.hpp>
 #include <tf2_geometry_msgs/tf2_geometry_msgs.hpp>
 #include <tf2_eigen/tf2_eigen.hpp>
 
@@ -479,7 +479,7 @@ static bool collectConstraints(const rclcpp::Node::SharedPtr& node, const std::v
     const auto constraint_param = "constraints." + constraint_id;
     if (!node->has_parameter(constraint_param + ".type"))
     {
-      RCLCPP_ERROR(LOGGER, "constraint parameter does not specify its type");
+      RCLCPP_ERROR(LOGGER, "constraint parameter \"%s\" does not specify its type", constraint_param.c_str());
       return false;
     }
     std::string constraint_type;
@@ -529,7 +529,7 @@ bool constructConstraints(const rclcpp::Node::SharedPtr& node, const std::string
     return false;
 
   for (auto& constraint_id : constraint_ids)
-    constraint_id.insert(0, constraints_param);
+    constraint_id.insert(0, constraints_param + std::string("."));
 
   return collectConstraints(node, constraint_ids, constraints);
 }
