a73b5352737e68f8b43c9adf0851cf2535c6c834
Henning Kayser <henningkayser@picknik.ai>
2023-09-10 22:40:43 +0200

    Rebase from 'upstream'

--- moveit_ros/planning/planning_scene_monitor/src/current_state_monitor_middleware_handle.cpp
+++ moveit_ros/planning/planning_scene_monitor/src/current_state_monitor_middleware_handle.cpp
@@ -51,10 +51,6 @@
 
 namespace planning_scene_monitor
 {
-namespace
-{
-static const auto SUBSCRIPTION_QOS = rclcpp::QoS(25);
-}
 
 CurrentStateMonitorMiddlewareHandle::CurrentStateMonitorMiddlewareHandle(const rclcpp::Node::SharedPtr& node)
   : node_(node)
@@ -70,7 +66,7 @@ void CurrentStateMonitorMiddlewareHandle::createJointStateSubscription(const std
                                                                        JointStateUpdateCallback callback)
 {
   joint_state_subscription_ =
-      node_->create_subscription<sensor_msgs::msg::JointState>(topic, SUBSCRIPTION_QOS, callback);
+      node_->create_subscription<sensor_msgs::msg::JointState>(topic, rclcpp::SystemDefaultsQoS(), callback);
 }
 
 void CurrentStateMonitorMiddlewareHandle::resetJointStateSubscription()
@@ -93,6 +89,11 @@ std::string CurrentStateMonitorMiddlewareHandle::getJointStateTopicName() const
 bool CurrentStateMonitorMiddlewareHandle::sleepFor(const std::chrono::nanoseconds& nanoseconds) const
 {
   return rclcpp::sleep_for(nanoseconds);
+}
+
+bool CurrentStateMonitorMiddlewareHandle::ok() const
+{
+  return rclcpp::ok();
 }
 
 void CurrentStateMonitorMiddlewareHandle::createDynamicTfSubscription(TfCallback callback)
