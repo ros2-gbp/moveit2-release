b26b93241536800163993a17fe3be7beefe2f166
Sebastian Castro <sebas.a.castro@gmail.com>
2024-11-29 10:20:23 -0500

    Rebase from 'debian/jazzy/moveit_ros_occupancy_map_monitor'

--- include/moveit/occupancy_map_monitor/occupancy_map_monitor_middleware_handle.h
+++ include/moveit/occupancy_map_monitor/occupancy_map_monitor_middleware_handle.h
@@ -13,7 +13,7 @@
 /*********************************************************************
  * Software License Agreement (BSD License)
  *
- *  Copyright (c) 2011, Willow Garage, Inc.
+ *  Copyright (c) 2021, PickNik, Inc.
  *  All rights reserved.
  *
  *  Redistribution and use in source and binary forms, with or without
@@ -26,7 +26,7 @@
  *     copyright notice, this list of conditions and the following
  *     disclaimer in the documentation and/or other materials provided
  *     with the distribution.
- *   * Neither the name of Willow Garage nor the names of its
+ *   * Neither the name of PickNik nor the names of its
  *     contributors may be used to endorse or promote products derived
  *     from this software without specific prior written permission.
  *
@@ -44,8 +44,8 @@
  *  POSSIBILITY OF SUCH DAMAGE.
  *********************************************************************/
 
-/* Author: Ioan Sucan, Jon Binney */
+/* Author: Tyler Weaver */
 
 #pragma once
 #pragma message(".h header is obsolete. Please use the .hpp header instead.")
-#include <moveit/occupancy_map_monitor/occupancy_map_updater.hpp>
+#include <moveit/occupancy_map_monitor/occupancy_map_monitor_middleware_handle.hpp>
