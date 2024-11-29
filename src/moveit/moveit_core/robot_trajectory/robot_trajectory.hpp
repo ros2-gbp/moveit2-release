c3f0d15502afb47bbaddf2bdbcad225eb3b2da8e
Sebastian Castro <sebas.a.castro@gmail.com>
2024-11-29 10:42:45 -0500

    Rebase from 'release/rolling/moveit_py'

--- src/moveit/moveit_core/robot_trajectory/robot_trajectory.hpp
+++ src/moveit/moveit_core/robot_trajectory/robot_trajectory.hpp
@@ -34,33 +34,28 @@
 
 /* Author: Peter David Fagan */
 
+#pragma once
+
 #include <pybind11/pybind11.h>
-#include <pybind11/eigen.h>
 #include <pybind11/stl.h>
+#include <pybind11/eigen.h>
+#include <moveit/robot_trajectory/robot_trajectory.hpp>
+#include <moveit_msgs/msg/robot_trajectory.hpp>
 #include <rclcpp/rclcpp.hpp>
-#include <moveit/robot_model/robot_model.h>
-#include <moveit_msgs/msg/constraints.hpp>
 
 namespace py = pybind11;
 
 namespace moveit_py
 {
-namespace bind_kinematic_constraints
+namespace bind_robot_trajectory
 {
-moveit_msgs::msg::Constraints constructLinkConstraint(const std::string& link_name, const std::string& source_frame,
-                                                      std::optional<std::vector<double>> cartesian_position,
-                                                      std::optional<double> cartesian_position_tolerance,
-                                                      std::optional<std::vector<double>> orientation,
-                                                      std::optional<double> orientation_tolerance);
+moveit_msgs::msg::RobotTrajectory
+getRobotTrajectoryMsg(const robot_trajectory::RobotTrajectoryConstPtr& robot_trajectory,
+                      const std::vector<std::string>& joint_filter);
+robot_trajectory::RobotTrajectory
+setRobotTrajectoryMsg(const std::shared_ptr<robot_trajectory::RobotTrajectory>& robot_trajectory,
+                      const moveit::core::RobotState& robot_state, const moveit_msgs::msg::RobotTrajectory& msg);
 
-moveit_msgs::msg::Constraints constructJointConstraint(moveit::core::RobotState& robot_state,
-                                                       moveit::core::JointModelGroup* joint_model_group,
-                                                       double tolerance);
-
-moveit_msgs::msg::Constraints constructConstraintsFromNode(const std::shared_ptr<rclcpp::Node>& node_name,
-                                                           const std::string& ns);
-
-void initKinematicConstraints(py::module& m);
-
-}  // namespace bind_kinematic_constraints
+void initRobotTrajectory(py::module& m);
+}  // namespace bind_robot_trajectory
 }  // namespace moveit_py
