d99aa0a532260d9e0b7a63e90bd44d1412243014
Nathan Brooks <nathanbrooks@picknik.ai>
2026-08-12 00:20:16 -0600

    Rebase from 'upstream'

--- moveit_ros/moveit_servo/demos/servo_keyboard_input.cpp
+++ moveit_ros/moveit_servo/demos/servo_keyboard_input.cpp
@@ -40,8 +40,10 @@
 
 #include <chrono>
 #include <control_msgs/msg/joint_jog.hpp>
+#include <cstdint>
 #include <geometry_msgs/msg/twist_stamped.hpp>
 #include <moveit_msgs/srv/servo_command_type.hpp>
+#include <rclcpp/executors/single_threaded_executor.hpp>
 #include <rclcpp/rclcpp.hpp>
 #include <signal.h>
 #include <stdio.h>
@@ -188,9 +190,11 @@ int main(int argc, char** argv)
 
 void KeyboardServo::spin()
 {
+  rclcpp::executors::SingleThreadedExecutor executor;
+  executor.add_node(nh_);
   while (rclcpp::ok())
   {
-    rclcpp::spin_some(nh_);
+    executor.spin_some();
   }
 }
 
