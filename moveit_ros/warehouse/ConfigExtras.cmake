c618a901ccfa848e65143b260112fde223338ed7
Nathan Brooks <nathanbrooks@picknik.ai>
2026-09-16 11:09:04 -0600

    Revert "Trimmed the branch to only the moveit_ros/moveit_ros sub directory"
    This reverts commit 23375343a51356ed8b28c06c68f85365a6d8860a.

---
+++ moveit_ros/warehouse/ConfigExtras.cmake
@@ -0,0 +1,4 @@
+# Extras module needed for dependencies to find boost components
+
+find_package(Boost CONFIG REQUIRED COMPONENTS thread filesystem regex date_time
+                                              program_options)
