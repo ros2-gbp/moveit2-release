7014095ef86027bd36d3c68939c968921d28d7fe
Nathan Brooks <nathanbrooks@picknik.ai>
2026-09-16 11:08:12 -0600

    Trimmed the branch to only the moveit_setup_assistant/moveit_setup_app_plugins sub directory

--- moveit_ros/planning/ConfigExtras.cmake
+++
@@ -1,4 +0,0 @@
-# Extras module needed for dependencies to find boost components
-
-find_package(Boost CONFIG REQUIRED COMPONENTS filesystem date_time
-                                              program_options thread chrono)
